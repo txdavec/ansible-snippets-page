69b576d2f3d056d8c4f06604b18dbb1a901bd3ff
txdavec <gosteveo@gmail.com>
2019-01-20 08:55:07 -0600

    add accordion code

--- build.sh
+++ build.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+#-------------------------------------------------------------------------------
+# Simple bash script to create a web page of ansible snippets using 
+# an html/css accordion
+# Accordion code courtesy of: https://www.w3schools.com/
+#
+# Copyright 2019 Dave Chouinard
+# MIT License
+#-------------------------------------------------------------------------------
+
 MODULES='apt blockinfile command copy cron debug file group lineinfile mount package pip script service shell stat template unarchive user yum'
-echo '<html>'
+cat begin.html
 for MODULE in ${MODULES}
 do
-  echo "<br/><hr/><h3><a href=\"https://docs.ansible.com/ansible/latest/modules/${MODULE}_module.html\">${MODULE}</a></h3><pre>"
+  echo "<button class=\"accordion\">${MODULE}</button>"
+  echo "<div class=\"panel\"><p><a target=\"_blank\" href=\"https://docs.ansible.com/ansible/latest/modules/${MODULE}_module.html\">${MODULE}</a><pre>"
   ansible-doc -s ${MODULE}
-  echo '</pre>'
+  echo '</pre></p></div>'
 done
-echo '</html>'
+cat end.html
+
+exit 0
